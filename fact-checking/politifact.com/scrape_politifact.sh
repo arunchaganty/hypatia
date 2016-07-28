e7c13e41dc755c2d2b9cbdb76603704a4afc5f98
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2016-07-27 17:24:05 -0700

    Changed id script in scrape politifact

--- fact-checking/politifact.com/scrape_politifact.sh
+++ fact-checking/politifact.com/scrape_politifact.sh
@@ -2,5 +2,5 @@
 PAGE_LIMIT=602 # Obtained through inspection.
 for i in `seq $PAGE_LIMIT`; do 
   curl http://politifact.com/truth-o-meter/statements/?page=$i | python3 scrape_politifact.py;
-done | csvt id --output politifact.tsv
+done | csvid --output politifact.tsv
 
