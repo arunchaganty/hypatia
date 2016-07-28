59821b7f1ddc8c830c67a4fd7f0a98201831420b
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-07-28 13:46:39 -0700

    Added basic views and templates

--- django/hypatia/urls.py
+++ django/hypatia/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from . import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('search.urls')),
 ]
+
+if settings.DEBUG:
+    from django.contrib import staticfiles
+    urlpatterns += [
+        url(r'^static/(?P<path>.*)$', staticfiles.views.serve),
+    ]
+
--- django/search/views.py
+++ django/search/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def search(request, query=""):
+    query = query.strip()
+    if len(query) == 0:
+        return redirect("/")
+
+    results = [
+        {
+            "id" : 0,
+            "statement" : "This is a statement",
+            "href" : "",},
+        {
+            "id" : 1,
+            "statement" : "This is a statement",
+            "href" : "",},
+        ]
+
+    return render(request, 'list.html', {'results':results})
+
